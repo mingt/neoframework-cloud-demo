3d8fc160da020d73ea76af6e8afc2fed7b8324a2
ahming <toahming@126.com>
2020-09-07 12:59:22 +0800

    base-common使用maven-publish发布后独立依赖

--- build-all-projects.sh
+++ build-all-projects.sh
@@ -2,5 +2,5 @@
 
 # build_type 有 release 或 test 等
 
-cd base-common; ./gradlew clean build -P build_type=release; cd ..
+cd base-common; ./gradlew clean build publishToMavenLocal -P build_type=release; cd ..
 cd trace-service; ./gradlew clean build -P build_type=release; cd ..
