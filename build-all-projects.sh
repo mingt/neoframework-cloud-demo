655e89890d9d50fc2a2f1faff0b0cf5fbb6b6caa
ahming <toahming@126.com>
2020-09-11 10:29:04 +0800

    之前遗漏未调整的base-common项目依赖改为独立发布依赖

--- build-all-projects.sh
+++ build-all-projects.sh
@@ -2,5 +2,12 @@
 
 # build_type 有 release 或 test 等
 
-cd base-common; ./gradlew clean build publishToMavenLocal -P build_type=release; cd ..
+cd config-server; ./gradlew clean build; cd ..
+cd webservice-registry; ./gradlew clean build; cd ..
+
+cd base-common; ./gradlew clean build publishToMavenLocal; cd ..
+
+cd auth-server-common; ./gradlew clean build; cd ..
+cd auth-server; ./gradlew clean build -P build_type=release; cd ..
+
 cd trace-service; ./gradlew clean build -P build_type=release; cd ..
